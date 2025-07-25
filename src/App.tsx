c544a6fd5025bfa02e5686e2bcc9e8e8ae8c0312
Seu Nome <Seu Nome>
2025-07-25 10:37:51 -0300

    Incluída a data atual.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 import { horarioAtual } from './utils/tempo'
+import { dataAtual } from './utils/tempo';
 
 function App() {
   const [tempoAtual, setTempoAtual] = useState(horarioAtual());
+  const [data, setData] = useState(dataAtual());
 
   useEffect(() => {
     const intervalo = setInterval(() => {
       setTempoAtual(horarioAtual());
+      setData(dataAtual());
     }, 1000);
 
     return () => clearInterval(intervalo);
   }, []);
 
+
   return (
     <>
       {/* Exibe o horário atual formatado */}
       <h1>Horário Atual: {tempoAtual}</h1>
+      <h1>Data Atual: {data}</h1>
+      
     </>
   )
 }
--- src/utils/tempo.ts
+++ src/utils/tempo.ts
@@ -7,3 +7,10 @@ export function horarioAtual(): string {
     return `${horas}:${minutos}:${segundos}`;
 }
 
+export function dataAtual(): string {
+    const agora = new Date();
+    const dia = agora.getDate().toString().padStart(2, '0');
+    const mes = (agora.getMonth() + 1).toString().padStart(2, '0');
+    const ano = agora.getFullYear();
+    return `${dia}/${mes}/${ano}`;
+}
